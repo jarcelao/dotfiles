bf4a93660236c474dccc561de49ac75548a222a9
Jericho Arcelao <jerichoarcelao@gmail.com>
2025-10-31 13:30:28 +0800

    feat: make config.fish safe

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,13 +1,38 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    set fish_greeting
+    set fish_greeting ""
+
+    if command -v eza >/dev/null 2>&1
+        alias e="eza -lah"
+    end
+
+    if command -v bat >/dev/null 2>&1
+        alias b="bat"
+    else if command -v batcat >/dev/null 2>&1
+        alias b="batcat"
+    end
+
+    if command -v starship >/dev/null 2>&1
+        starship init fish | source
+    end
 end
 
-fish_add_path $HOME/.local/bin/
+if test -d "$HOME/.local/bin"
+    fish_add_path "$HOME/.local/bin"
+end
 
-# bun
-set --export BUN_INSTALL "$HOME/.bun"
-set --export PATH $BUN_INSTALL/bin $PATH
+if command -v nvim >/dev/null 2>&1
+    set -gx EDITOR nvim
+    alias n="nvim"
+end
 
-zoxide init fish | source
-starship init fish | source
+if test -d "$HOME/.bun"
+    set --export BUN_INSTALL "$HOME/.bun"
+    if test -d "$BUN_INSTALL/bin"
+        set --export PATH "$BUN_INSTALL/bin" $PATH
+    end
+end
+
+if command -v zoxide >/dev/null 2>&1
+    zoxide init fish | source
+end
