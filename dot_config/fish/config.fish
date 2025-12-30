ee26f61c344b5f4d19734a3b05592f3954800449
Jericho Arcelao <jerichoarcelao@gmail.com>
2025-12-30 14:02:37 +0800

    fix: convert `fish_add_path` calls to global

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -31,7 +31,7 @@ if status is-interactive
 end
 
 if test -d "$HOME/.local/bin"
-    fish_add_path "$HOME/.local/bin"
+    fish_add_path -g "$HOME/.local/bin"
 end
 
 if command -v hx >/dev/null 2>&1
