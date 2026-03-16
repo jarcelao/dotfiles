d361202504cfd2a25df03329ea0138e361ddf9f7
Jericho Arcelao <jerichoarcelao@gmail.com>
2026-03-17 00:02:07 +0800

    fix: wezterm tab bar

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,14 +1,15 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local config = {}
 
 config.color_scheme = "catppuccin-macchiato"
-config.font = wezterm.font('JetBrains Mono')
-config.hide_tab_bar_if_only_one_tab = true
+config.font = wezterm.font("JetBrains Mono")
 
-if wezterm.target_triple == 'x86_64-unknown-linux-gnu' then
-  config.default_prog = { 'tmux' }
-elseif wezterm.target_triple == 'aarch64-apple-darwin' then
-  config.default_prog = { '/opt/homebrew/bin/tmux' }
+if wezterm.target_triple == "x86_64-unknown-linux-gnu" then
+	config.default_prog = { "tmux" }
+	config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
+elseif wezterm.target_triple == "aarch64-apple-darwin" then
+	config.default_prog = { "/opt/homebrew/bin/tmux" }
+	config.hide_tab_bar_if_only_one_tab = true
 end
 
 return config
