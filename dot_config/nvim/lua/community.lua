e815bec095f064364e9b0786b49ea6b390b3911c
Jericho Arcelao <jerichoarcelao@gmail.com>
2026-06-01 14:35:45 +0800

    feat: nvim python

--- dot_config/nvim/lua/community.lua
+++ dot_config/nvim/lua/community.lua
@@ -8,6 +8,7 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.colorscheme.catppuccin" },
   -- import/override with your plugins folder
 }
