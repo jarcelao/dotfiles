cf302a1a3f5536b2d208786cc45012c0fbb2eaac
Jericho Arcelao <jerichoarcelao@gmail.com>
2026-08-10 09:38:46 +0800

    feat: migrate from bash to fish

--- README.md
+++ README.md
@@ -28,13 +28,11 @@ Installing these applications depends on the OS and package manager.
 
 Install the [Ghostty terminal](https://ghostty.org).
 
-### Shell: `bash`
+### Shell: `fish`
 
-`bash` should likely be available by default.
+Install the [fish shell](https://fishshell.com/). Set it as the login shell with `chsh`.
 
-In this config, `~/.bashrc` contains general configurations, while `~/.bashrc.d/` contains machine-specific ones.
-
-On top of `bash`, I use [flyline](https://github.com/HalFrgrd/flyline) for a fish-like experience and [starship](https://starship.rs) for a pretty prompt. Ensure that these tools are available before applying the dotfiles.
+On top of this, I use [starship](https://starship.rs) for a pretty prompt. Ensure that this is available before applying the dotfiles.
 
 ### Editor: `nvim`
 
---
+++ dot_config/fish/config.fish
@@ -0,0 +1,42 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    set fish_greeting ""
+    
+    function y
+        set tmp (mktemp -t "yazi-cwd.XXXXXX")
+        yazi $argv --cwd-file="$tmp"
+        if read -z cwd <"$tmp"; and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+            builtin cd -- "$cwd"
+        end
+        rm -f -- "$tmp"
+    end
+
+    if command -v zoxide >/dev/null 2>&1
+        zoxide init fish | source
+    end
+
+    if command -v starship >/dev/null 2>&1
+        starship init fish | source
+    end
+
+    if command -v eza >/dev/null 2>&1
+        alias e="eza -lah"
+    end
+
+    if command -v bat >/dev/null 2>&1
+        alias b="bat"
+    end
+
+    if command -v lazygit >/dev/null 2>&1
+        alias lg="lazygit"
+    end
+end
+
+if command -v mise >/dev/null 2>&1
+  mise activate fish | source
+end
+
+if command -v nvim >/dev/null 2>&1
+    alias n="nvim"
+    set -gx EDITOR "nvim"
+end
